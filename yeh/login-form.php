640312afc704eda0c086895fe9213bd47aa2b49a
ChienChihYeh <miroyeh1986@gmail.com>
2022-09-23 12:43:10 +0800

    fix login-form.php

--- yeh/login-form.php
+++ yeh/login-form.php
@@ -52,10 +52,8 @@ if (!empty($_SESSION['admin'])) {
             alert("登入成功");
             // swal("登入成功!", "", "success");
             // location.href = 'members-list.php';
-            if (<?= !empty($_GET['from'])? true: false; ?>) {
-                location.href = '<?= isset($_GET['from'])? $_GET['from'] : "#"; ?>';
-            } 
-            location.href = 'members-list.php';
+                location.href = '<?= isset($_GET['from'])? $_GET['from'] : 'members-list.php'; ?>';
+            // location.href = 'members-list.php';
             // location.reload();
 
         } else {
