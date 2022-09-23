48ed133924f075d43ce82e9172d2fdc5555957cf
ChienChihYeh <miroyeh1986@gmail.com>
2022-09-23 15:25:20 +0800

    nav change

--- yeh/parts/nav.php
+++ yeh/parts/nav.php
@@ -2,7 +2,7 @@
     <div class="container">
         <nav class="navbar navbar-expand-lg bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand">管理後台</a>
+                <a class="navbar-brand" href="login-form-m.php">管理後台</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
