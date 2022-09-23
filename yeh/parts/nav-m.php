fbd2c78078c4b1e49660d008be169f2829a4b9dd
ChienChihYeh <miroyeh1986@gmail.com>
2022-09-23 15:31:05 +0800

    nav-m.php link

--- yeh/parts/nav-m.php
+++ yeh/parts/nav-m.php
@@ -33,8 +33,8 @@
 <div class="container">
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
-            <h1 class="logo">爬山趣</h1>
-            <a class="navbar-brand">會員介面</a>
+            <a href="login-form.php"><h1 class="logo">爬山趣</h1></a>
+            <!-- <a class="navbar-brand">會員介面</a> -->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
